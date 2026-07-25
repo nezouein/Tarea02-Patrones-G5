defac330410ee821e51a40bf1f4bc4245b3672ae
jnavarrete2026 <josnavar@espol.edu.ec>
2026-07-25 18:42:04 -0500

    Update CreadorPronosticoBaloncesto.java

--- src/com/sportspredictor/shared/factoryMethod/CreadorPronosticoBaloncesto.java
+++ src/com/sportspredictor/shared/factoryMethod/CreadorPronosticoBaloncesto.java
@@ -5,7 +5,7 @@ import com.sportspredictor.shared.EventoBaloncesto;
 import com.sportspredictor.shared.Pronostico;
 import com.sportspredictor.shared.Usuario;
 
-public class CreadorPronosticoBaloncesto extends CreadorPronostico {
+public class CreadorPronosticoBaloncesto extends creadorPronostico {
 
     @Override
     public Pronostico crearPronostico(Evento evento, Usuario usuario, Object datos) {
@@ -13,6 +13,6 @@ public class CreadorPronosticoBaloncesto extends CreadorPronostico {
         validarEventoAbierto(eventoBaloncesto);
 
         String prediccionGanador = convertirDatos(datos, String.class);
-        return new PronosticoBaloncesto(eventoBaloncesto, usuario, prediccionGanador);
+        return new pronosticoBaloncesto(eventoBaloncesto, usuario, prediccionGanador);
     }
 }

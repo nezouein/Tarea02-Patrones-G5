6a460c3f5ebc6c44463c43ecbb7e6c24a45b1b0c
jnavarrete2026 <josnavar@espol.edu.ec>
2026-07-25 18:41:22 -0500

    Update creadorPronosticoTenis.java

--- src/com/sportspredictor/shared/factoryMethod/creadorPronosticoTenis.java
+++ src/com/sportspredictor/shared/factoryMethod/creadorPronosticoTenis.java
@@ -1,5 +1,17 @@
 package com.sportspredictor.shared.factoryMethod;
 
-public class creadorPronosticoTenis {
+import com.sportspredictor.shared.Evento;
+import com.sportspredictor.shared.EventoTenis;
+import com.sportspredictor.shared.Pronostico;
+import com.sportspredictor.shared.Usuario;
 
+public class creadorPronosticoTenis extends creadorPronostico {
+    @Override
+    public Pronostico crearPronostico(Evento evento, Usuario usuario, Object datos) {
+        EventoTenis eventoTenis = convertirDatos(evento, EventoTenis.class);
+        validarEventoAbierto(eventoTenis);
+
+        String prediccionGanador = convertirDatos(datos, String.class);
+        return new pronosticoTenis(eventoTenis, usuario, prediccionGanador);
+    }
 }

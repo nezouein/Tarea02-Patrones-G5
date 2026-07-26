19775cf5ab1281aa5a7c0127de649798052dab92
Angie_Inga <ingangie8@gmail.com>
2026-07-26 01:24:00 +0000

    1

---
+++ src/com/sportspredictor/shared/Observer/PushObserver.java
@@ -0,0 +1,17 @@
+
+public class PushObserver implements ObservadorNotificacion {
+    private String tokenDispositivo;
+
+    public PushObserver(String tokenDispositivo) {
+        this.tokenDispositivo = tokenDispositivo;
+    }
+
+    @Override
+    public void actualizar(Notificacion notificacion) {
+        enviarPush(notificacion);
+    }
+
+    private void enviarPush(Notificacion notificacion) {
+        System.out.println("[Push -> " + tokenDispositivo + "] " + notificacion);
+    }
+}
